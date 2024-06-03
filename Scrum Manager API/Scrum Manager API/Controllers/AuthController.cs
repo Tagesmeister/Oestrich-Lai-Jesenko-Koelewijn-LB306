d183bb1510a2fb7f77dd85e06a4f071c2ec0f2c2
K.Koelewijn.inf22 <K.koelewijn.inf22@stud.bbbaden.ch>
2024-06-03 16:23:22 +0200

    Authcontroller implementation

--- Scrum Manager API/Scrum Manager API/Controllers/AuthController.cs
+++ Scrum Manager API/Scrum Manager API/Controllers/AuthController.cs
@@ -1,17 +1,64 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Scrum_Manager_API.Models;
 
 namespace Scrum_Manager_API.Controllers;
 
-public class AuthController
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    public ActionResult Register(Login login)
+    public static Login user = new();
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
     {
-        // Logic to register a new user
+        _configuration = configuration;
     }
 
-    public ActionResult Login(Login login)
+    [HttpPost("register")]
+    public ActionResult<Login> Register(LoginDTO request)
     {
-        // Logic to authenticate a user
+        var passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+
+        user.Username = request.UserName;
+        user.PasswordHash = passwordHash;
+
+        return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public ActionResult<Login> Login(LoginDTO request)
+    {
+        if (user.Username != request.UserName || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+        {
+            return BadRequest("Credentials are false. Please Try Again.");
+        }
+
+        var token = CreateToken(user);
+
+        return Ok(token);
+    }
+
+    private string CreateToken(Login user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: creds);
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
     }
 }
