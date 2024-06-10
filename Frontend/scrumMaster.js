c118d60a8c956ffa59cd6d237708eb9ed8349dda
Tagesmeister <110892258+Tagesmeister@users.noreply.github.com>
2024-06-10 16:13:44 +0200

    Add files via upload

---
+++ Frontend/scrumMaster.js
@@ -0,0 +1,28 @@
+const apiURL = ""
+
+function PostScrumLog(){
+    let inputText = document.getElementById('input-log').value;
+    let inputTitle = document.getElementById('input-title').value;
+
+    const formData = new FormData()
+    
+    formData.append('title', inputTitle);
+    formData.append('description', inputText);
+
+    formData.forEach((value, key) => {
+        console.log(`${key}: ${value}`);
+    });
+
+    fetch(apiURL,{
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data =>{
+        console.log('Sucess:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);   
+        alert('Error submitting form!');
+      });
+}
